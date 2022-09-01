568480ae221ca8a6b6570e4987e6ea55ca773e76
Arthur Moreira <arthurmcs96@gmail.com>
2022-09-01 19:19:59 -0300

    muda o background

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
